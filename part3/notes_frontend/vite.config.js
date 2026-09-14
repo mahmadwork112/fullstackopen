e300f5f36e40ce1510cb10804088199c9999814c
mahmadwork112 <m.ahmad.work112@gmail.com>
2026-09-15 01:20:08 +0500

    bug: https -> http

--- part3/notes_frontend/vite.config.js
+++ part3/notes_frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://localhost:3001",
+        target: "http://localhost:3001",
         changeOrigin: true,
       },
     },
