81cb8ddb7fd85e75fe998ed2378971a6570c08ce
mahmadwork112 <m.ahmad.work112@gmail.com>
2026-09-16 00:09:11 +0500

    trying a solution

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -104,6 +104,6 @@ app.post("/api/persons", (request, response) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
