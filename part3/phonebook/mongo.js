f655ebec20e0b2e8b290a5e82ce0770953a4b0e5
mahmadwork112 <m.ahmad.work112@gmail.com>
2026-09-18 15:01:30 +0500

    added mongoose and mongo.js.

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+// ask for password if not provided
+if (process.argv.length < 3) {
+  console.log("give password first");
+  process.exit(1);
+}
+
+// saving the password
+const password = process.argv[2];
+
+// url to the database
+const url = `mongodb+srv://mahmadsiddiqui111_db_user:${password}@phonebookcluster.7tqbq90.mongodb.net/?appName=PhonebookCluster`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url, { family: 4 });
+
+// each person needs to have a name and a phone number which will both be of type string
+// beacuse of the '-' in the numbers
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+// const person = new Person({
+//   name: "Ahmad",
+//   number: "123456789",
+// });
