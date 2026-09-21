87e7f0c079de0feb7e5b6e1e42ac4cee98eba7a2
mahmadwork112 <m.ahmad.work112@gmail.com>
2026-09-21 21:58:19 +0500

    removing hardcoded password

--- part3/notes/mongo.js
+++ part3/notes/mongo.js
@@ -7,8 +7,7 @@ if (process.argv.length > 3) {
 
 const password = process.argv[2];
 
-const url =
-  "mongodb+srv://mahmadsiddiqui111_db_user:QoLsQNp8phDZ2AQG@cluster0.pcm2um9.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0";
+const url = `mongodb+srv://mahmadsiddiqui111_db_user:${password}@cluster0.pcm2um9.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.set("strictQuery", false);
 
