a64cde4d5484993347d5c7b06147528b4dac120a
mahmadwork112 <m.ahmad.work112@gmail.com>
2026-09-26 14:13:10 +0500

    using dotenv for environment variables. such as MONGODB_URI and PORT number.

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
@@ -90,7 +91,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
