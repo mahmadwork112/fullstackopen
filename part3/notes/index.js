be95130ce1c08e284bbc2cec2d9e976fbdbdd78b
mahmadwork112 <m.ahmad.work112@gmail.com>
2026-09-26 14:20:41 +0500

    fixed saving a note, according to mongodb.

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -67,15 +67,16 @@ app.post("/api/notes", (request, response) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
   notes = notes.concat(note);
 
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(note);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
